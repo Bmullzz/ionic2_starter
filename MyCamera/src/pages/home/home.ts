31f5a4b475d7bd5c6cde35a34e6103ff1d05eadc
Froilan Miranda <froilan.miranda@gmail.com>
2017-06-28 16:39:55 -0400

    update Camera demo

--- MyCamera/src/pages/home/home.ts
+++ MyCamera/src/pages/home/home.ts
@@ -1,31 +1,32 @@
 import { Component, Input } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Camera } from 'ionic-native';
+import { Camera } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+	providers: [Camera]
+
 })
-
 export class HomePage {
-	public imageData:string;
-	@Input('useURI') useURI:Boolean = true;
+	public imageData:String;
+	@Input('useURI') useURI: Boolean = true;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, private camera:Camera) {}
 
 	getPicture(sourceType){
-		Camera.getPicture({
-			quality: 50,
-			allowEdit: true,
-			encodingType: Camera.EncodingType.JPEG,
-			saveToPhotoAlbum:false,
-			destinationType: this.useURI ? Camera.DestinationType.FILE_URI : Camera.DestinationType.DATA_URL,
-			targetWidth: 800,
-			targetHeight: 800,
+		this.camera.getPicture({
+			quality:50,
+			allowEdit:true,
+			encodingType: this.camera.EncodingType.JPEG,
+			saveToPhotoAlbum: false,
+			destinationType: this.useURI ? this.camera.DestinationType.FILE_URI: this.camera.DestinationType.DATA_URL,
+			targetWidth:800,
+			targetHeight:800,
 			sourceType: sourceType
 		}).then((imageData) => {
-			if (this.useURI){
-				this.imageData = imageData;
+			if(this.useURI){
+				this.imageData=imageData;
 			}else{
 				this.imageData = "data:image/jpeg;base64," + imageData;
 			}
